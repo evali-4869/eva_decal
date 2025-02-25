a96c19a9d6a04abb908316dbda3f5546f2a519aa
Eva Li <evali@Evas-MacBook-Air.local>
2025-02-24 21:30:43 -0800

    add hw2

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -1,10 +1,23 @@
 # 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+# cd ~/desktop
+
 # 2) What does ~/ mean?
+# home directory
+
 # 3) What's the difference between and abolute and a relative path in your terminal?
+# An absolute path is a path from the home directory, a relative path is a path from current working directory.
+
 # 4) What command would bring you back to your home directory?
+# cd 
+
 # 5) If you called rm ./ in your current working directory, what would it do? 
+# It won't do anything becuase there's no specific name of the file or directory to be removed.
+
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+# "git add" stage the changes in my working directory, "git commit" save the staged changes to my local repository, "git push" upload the commits to the remote repository. 
+
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+# 
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
 # 9) How does cloning a repository differ from pulling from a repository?
 # 10) Tell me a fun fact!
