ec682a7aa1359f12af3331cced503668d441a423
Eva Li <evali@Evas-MacBook-Air.local>
2025-02-24 21:44:30 -0800

    add hw2

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -17,7 +17,13 @@
 # "git add" stage the changes in my working directory, "git commit" save the staged changes to my local repository, "git push" upload the commits to the remote repository. 
 
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
-# 
+# it would show "changes to be commited" with the name of the file that have been staged and is ready to be commited.
+
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+# syntax error has been the most frustrating because it's still hard for me to remember them and when I encounter an error I had to look back to my notes or google to find the corect syntax. 
+
 # 9) How does cloning a repository differ from pulling from a repository?
+# cloning is copying an entire remote repository to my local computer, pulling is getting the updates from the remote repository into my existing local repository.
+
 # 10) Tell me a fun fact!
+# A banana is actually a berry since it develops from a single ovary of a flower and contains seeds.
